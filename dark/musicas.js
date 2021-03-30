44cec1ee90c14478b1b20f4ad72dd848ecf20c7a
Mariahackooo <81647762+Mariahackooo@users.noreply.github.com>
2021-03-30 15:44:08 -0400

    Add files via upload

---
+++ dark/musicas.js
@@ -0,0 +1,35 @@
+const musicas = () => { 
+	return `           
+──────────────────
+*YOUTUBE MUSIC*
+──────────────────
+_*COMANDOS:*_
+──────────────────
+
+1• *${prefix}hylander*
+2• *${prefix}nikeb*
+3• *${prefix}BanhoDeLeite*
+4• *${prefix}AkDoFlamengo*
+5• *${prefix}Nemo*
+6• *${prefix}kalidade*
+7• *${prefix}macaverde*
+8• *${prefix}m4*
+9• *${prefix}cmgremio*
+10• *${prefix}cmflamengo*
+11• *${prefix}mercury*
+12• *${prefix}ponta*
+13• *${prefix}moça*
+14• *${prefix}nav*
+15• *${prefix}dama*
+16• *${prefix}remedy*
+17• *${prefix}cavala*
+18• *${prefix}facetime*
+19• *${prefix}pe*
+20• *${prefix}nike12*
+
+──────────────────
+『 PimPim 🤝 』
+           
+──────────────────`
+}
+exports.musicas = musicas
